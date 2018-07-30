0220f932507fdf98914b86ba3abcf8b19d5f135d
Imipenem <philipp.ehmele@yahoo.de>
2018-07-30 20:49:24 +0200

    Initial commit:
    -This application provides a working User LoginScreen with a password and a name
    -The UserData is stored in a JSON-File using the GSON-library
    
    -Need for constraints for a flexible seizing layout

---
+++ src/maindir/User.java
@@ -0,0 +1,51 @@
+
+public class User {
+    private String password;
+    private String Username;
+
+    User(String username, String password) {
+        this.password = password;
+        Username = username;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof User))return false;
+        User otherUser = (User)other;
+
+        return otherUser.getUsername().equals(Username) && otherUser.getPassword().equals(password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + Username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return "Username: "+getUsername()+" und Passwort: "+getPassword();
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+}
+
